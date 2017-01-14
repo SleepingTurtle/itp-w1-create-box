d1b833550aff290c4c446b99436ff6498990c659
Joshua Charles <joshcharlestechie@gmail.com>
2017-01-14 17:56:51 +0000

    main and tests completed

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,13 @@
 
 
 def create_box(height, width, character):
-    pass
+    box =""
+    for counter in range(height):
+        box += (character * width)
+        box += '\n'
+    
+    return box
 
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    create_box(5, 24, 'x')
